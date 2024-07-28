2eabbefdc291a593fe7266704d6304de987e0ac9
YHSYusuf <114289365+YHSYusuf@users.noreply.github.com>
2024-07-28 19:46:16 +0300

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
+
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.executeSlash(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    },
+};
